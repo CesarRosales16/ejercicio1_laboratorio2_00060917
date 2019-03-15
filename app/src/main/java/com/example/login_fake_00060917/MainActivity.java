6d3129cd323f89d4d6d38eac244cfbf00e45de89
CesarRosales16 <00060917@uca.edu.sv>
2019-03-15 10:06:53 -0600

    Terminado 1

---
+++ app/src/main/java/com/example/login_fake_00060917/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.login_fake_00060917;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText username,password;
+    private Button button;
+    private TextView text_show;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        button=findViewById(R.id.button);
+        text_show=findViewById(R.id.text_show);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_show.setText(username.getText());
+            }
+        });
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                text_show.setText(password.getText());
+                return false;
+            }
+        });
+    }
+}
